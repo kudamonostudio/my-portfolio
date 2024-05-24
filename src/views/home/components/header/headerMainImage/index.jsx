8101f939d84b29d0137e8c944771bbd7fd5bf018
martingdinardi <martingdinardi@gmail.com>
2024-05-24 15:43:27 -0300

    MYPO-01: Menu appears with animation effect

--- src/views/home/components/header/headerMainImage/index.jsx
+++ src/views/home/components/header/headerMainImage/index.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import "./index.css"
+import "./index.css";
 
 function HeaderMainImage() {
   const testImages = ["7476881", "3656518", "20788940", "672489"];
@@ -9,17 +8,17 @@ function HeaderMainImage() {
   useGSAP(() => {
     gsap.fromTo(
       ".header-image-container",
-      { height: "10px" },
-      { height: "800px", duration: 1.7, ease: "power2.out" }
+      { height: "1vh" },
+      { height: "90vh", duration: 1.7, ease: "power2.out" }
     );
   });
   return (
-    <div className="header-right w-1/2 flex justify-end items-end h-full max-h-[910px] absolute right-0">
-      <div className="overflow-hidden header-image-container absolute bottom-0 w-full">
+    <div className="header-right w-1/2 flex justify-end items-end absolute h-[100vh] right-0">
+      <div className="header-image-container w-full absolute overflow-hidden bottom">
         <img
           src={`https://images.pexels.com/photos/${testImage}/pexels-photo-${testImage}.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load`}
           alt="Profile Photo"
-          className="h-[800px] absolute bottom-0 right-0"
+          className="max-h-[35rem] h-[35rem] absolute bottom-0 right-0 header-image"
         />
       </div>
     </div>
